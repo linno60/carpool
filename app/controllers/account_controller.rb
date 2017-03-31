aabce5cda5cff8dcb63be39aa589a629841ebf1b
caitlynyang <caitlynyang@users.noreply.github.com>
2017-03-30 22:17:44 -0700

    Add files via upload

---
+++ app/controllers/account_controller.rb
@@ -0,0 +1,22 @@
+class AccountController < ApplicationController
+
+  def index
+    redirect_to account_profile_path
+  end
+
+  def profile
+  end
+
+  def organisation
+  end
+
+  def email_and_password
+  end
+
+  def carpooling_preferences
+  end
+
+  def upgrade_cancel
+  end
+
+end
